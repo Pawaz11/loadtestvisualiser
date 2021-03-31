d25cf2c15f5358e4eddb909a5db9e27d96524968
Pawan Kumar <Pawan_Kumar@Steris.com>
2021-03-31 13:11:06 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Load_Test_Visualiser.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
