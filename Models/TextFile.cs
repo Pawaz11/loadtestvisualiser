7095d54ca6b016c3cb57180909b4989973a9f174
Pawan Kumar <Pawan_Kumar@Steris.com>
2021-03-31 13:35:31 +0100

    Add TextFile model

---
+++ Models/TextFile.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Load_Test_Visualiser.Models
+{
+    public class TextFile
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
