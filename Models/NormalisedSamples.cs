840c71bacae8ecc972070a96473926eb1a60aa94
Pawan Kumar <Pawan_Kumar@Steris.com>
2021-04-20 13:26:28 +0100

    Add NormalisedSamples class

---
+++ Models/NormalisedSamples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Load_Test_Visualiser.Models
+{
+    public class NormalisedSamples
+    {
+        public string ThreadName { get; set; }
+        public string Label { get; set; }
+        public long StartTime { get; set; }
+        public long EndTime { get; set; }
+        public bool Failure { get; set; }
+        public string ResponseMessage { get; set; }
+    }
+}
